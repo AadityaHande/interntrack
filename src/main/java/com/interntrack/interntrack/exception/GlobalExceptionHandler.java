7bbf6daf35006543bae82b7ce299f34b9be55719
AadityaHande <aadityahande27@gmail.com>
2026-09-09 23:32:05 +0530

    duplicate import fix

--- src/main/java/com/interntrack/interntrack/exception/GlobalExceptionHandler.java
+++ src/main/java/com/interntrack/interntrack/exception/GlobalExceptionHandler.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
